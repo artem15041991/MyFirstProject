af2cfe022073be69855eb11ce8f79a85cce03d06
Artem Karpov <artem15041991@gmail.com>
2015-07-18 14:30:41 +0600

    Initial Commit

---
+++ MyFirstProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyFirstProject
+//
+//  Created by Home on 7/18/15.
+//  Copyright (c) 2015 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
